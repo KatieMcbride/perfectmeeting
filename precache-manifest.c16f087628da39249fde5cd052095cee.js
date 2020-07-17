46f29c759ec4167f4564a90011d9e5175231024d
Katie McBride <mcbride.katieb@gmail.com>
2020-07-16 20:47:19 -0500

    Updates

---
+++ precache-manifest.c16f087628da39249fde5cd052095cee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19aa9ea9c8ad3244b9e051ddc7dc4bed",
+    "url": "/perfectmeeting/index.html"
+  },
+  {
+    "revision": "2e67f147372143075a67",
+    "url": "/perfectmeeting/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c872593164a128cbf5fd",
+    "url": "/perfectmeeting/static/js/2.0b4cff1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/perfectmeeting/static/js/2.0b4cff1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e67f147372143075a67",
+    "url": "/perfectmeeting/static/js/main.fe8fc028.chunk.js"
+  },
+  {
+    "revision": "5c437e6b333d00f732b7",
+    "url": "/perfectmeeting/static/js/runtime-main.86ec8e29.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/perfectmeeting/static/media/logo.5d5d9eef.svg"
+  }
+]);
