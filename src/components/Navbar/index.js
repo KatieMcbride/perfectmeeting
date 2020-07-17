9f4f215cc24de2241b8282d33c6aff06de33b163
Katie McBride <mcbride.katieb@gmail.com>
2020-07-17 14:14:15 -0500

    navbar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import Logo from '../../assets/img/updated BAE.png';
+import './style.css';
+
+function Navbar() {
+  return (
+    <nav>
+        This is the NavBar
+
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+import './style.css';
+import Navbar from '../components/Navbar';
+
+
+function Landing() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default Landing;
